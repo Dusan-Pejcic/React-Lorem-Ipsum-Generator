32711c243114edcd32447ffbeeabccca05993ef4
dusan pejcic <dushan.pejcic@googlemail.com>
2021-09-22 15:03:17 +0200

    functions to fetch data and handle submit added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+const url = 'https://hipsum.co/api/?type=hipster-centric&paras=20';
 function App() {
   const [count, setCount] = useState(0);
-  const [text, setText] = useState([])
+  const [text, setText] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async ()=> {
+    setLoading(true);
+    const response = await fetch(url);
+    const fetchedData = await response.json();
+    setText(fetchedData);
+    setLoading(false);
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e);
+    fetchData();
   }
+
   return (
     <section className="section-center">
       <h3>tired of boring lorem ipsum?</h3>
+
       <form action="" className="lorem-form" onSubmit={handleSubmit}>
         <label htmlFor="amount">
           paragraphs:
@@ -17,9 +29,12 @@ function App() {
         <input type="number" name='amount' id='amount' value={count} onChange={(e)=> setCount(e.target.value)}/>
         <button type='submit' className='btn'>generate</button>
       </form>
+
       <article className="lorem-text">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, maiores distinctio. A illum, neque voluptatem non quia tempora expedita rem provident maxime accusantium excepturi at veniam beatae voluptatum totam dignissimos.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, maiores distinctio. A illum, neque voluptatem non quia tempora expedita rem provident maxime accusantium excepturi at veniam beatae voluptatum totam dignissimos.</p>
+        {loading && <p>loading...</p>}
+        {text.map((paragraph, index)=> {
+          return (<p key={index}>{paragraph}</p>)
+        })}
       </article>
     </section>
   );
